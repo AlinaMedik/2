39d27a8a4d3ad2f60029b30c442e3f81212f0500
Сладкомедова Алина Андреевна <ri58s16@awp11.csc.local>
2016-03-22 09:30:55 +0600

    вносим изменения в коммит

--- main.c
+++ main.c
@@ -3,4 +3,15 @@
 
 int main()
 {
+    double d, a, b, c, x;
+
+    printf ("wedite koeffizient 1\n");
+    scanf ("%d", &a);
+
+    printf ("wedite koeffizient 2\n");
+    scanf ("%d", &b);
+
+    printf ("wedite koeffizient 3\n");
+    scanf ("%d", &c);
+
 }
