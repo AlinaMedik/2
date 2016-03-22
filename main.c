9a0498efab555bbb45ee1cac0f509c7856933ef0
Сладкомедова Алина Андреевна <ri58s16@awp11.csc.local>
2016-03-22 09:36:13 +0600

    вносим изменения в коммит

--- main.c
+++ main.c
@@ -3,20 +3,32 @@
 
 void kv(double d, double a,  double b,  double c)
 {
-		
+	if (d >= 0)
+    {
+        if (d > 0)
+        {
+            printf("x 1 = %d", (-b - d) / 2 * a);
+            printf("x 2 = %d", (-b + d) / 2 * a);
+        }
+
+        printf("x = %d", (-b / 2 * a);
+    }
+
+    printf("net recheni");
+	
 }
 int main()
 {
     double d, a, b, c, x;
 
-    printf ("wedite koeffizient 1\n");
-    scanf ("%d", &a);
+    printf("wedite koeffizient 1\n");
+    scanf("%d", &a);
 
-    printf ("wedite koeffizient 2\n");
-    scanf ("%d", &b);
+    printf("wedite koeffizient 2\n");
+    scanf("%d", &b);
 
-    printf ("wedite koeffizient 3\n");
-    scanf ("%d", &c);
+    printf("wedite koeffizient 3\n");
+    scanf("%d", &c);
 
 	d = sqrt(b * b - 4 * a * c);
 	
